5853c44ba1110a172f99106e4bf0e7ad4f0e2560
lalitha <lalithapelluri6@gmail.com>
2019-04-29 22:39:14 -0700

    some more changes

---
+++ client/build/precache-manifest.dddaf224b89a374d901c1b21882abd9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57c60a6217785c4ea8e4",
+    "url": "/static/css/main.d8c7ea4e.chunk.css"
+  },
+  {
+    "revision": "57c60a6217785c4ea8e4",
+    "url": "/static/js/main.7fef8504.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "511f1beb012fccae7656",
+    "url": "/static/js/2.bdf1e34b.chunk.js"
+  },
+  {
+    "revision": "371756ae424eb147a58808db86582d00",
+    "url": "/index.html"
+  }
+];
